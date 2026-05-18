2d9a7221caa7b44cf3b5035606953fa41d098638
arushiiii18 <arushisagar18@gmail.com>
2026-05-18 06:12:02 +0530

    Prepared project for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,7 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use("/api/candidates", candidateRoutes);
 app.use("/api", matchRoutes);
-
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
